0b23f9eb3fcde182cfdcee4ea30e4cea0a6aa21c
20024416 <n.dinsbach@gmail.com>
2023-03-23 12:41:25 +0100

    added collection to db.json

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// const jsonServer = require('json-server')
+// const app = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const.middlewares = jsonServer.defaults()
+
+
 const path = require('path')
 const stampRoutes = require('./routes/stamps')
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, "public")))
+// execute middle-ware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json()) // parse JSON
+app.use(express.static(path.join(__dirname, "public")))  // create static public folder
 
+// step through the routes in routes folder
 app.use('/', stampRoutes)
+
+// catch all | send 404
+app.use((res, req, next) => {
+    res.statusCode(404);
+
+})
+
 
 app.listen(3001);
