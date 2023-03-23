b9cd7836d161e12220ab362f9c9ffb5cde74811c
20024416 <n.dinsbach@gmail.com>
2023-03-23 16:30:04 +0100

    added and implemented simple POST route, added status 200

--- routes/stamps.js
+++ routes/stamps.js
@@ -7,42 +7,60 @@ router.get('/stamps/:id', (req, res, next) => {
     const id = parseInt(req.params.id);
     const stamp = db.stamps.find(s => s.id === id);
     if (stamp) {
-      res.json(stamp);
+        res.status(200).json(stamp);
     } else {
-      res.status(404).send(`Stamp with ID ${id} does not exist in Nick's Stamp collection.. Yet.. Gotta catch 'em all!`);
+        res.status(404).send(`Stamp with ID ${id} does not exist in Nick's Stamp collection.. Yet.. Gotta catch 'em all!`);
     }
-  });
+});
 
 router.delete('/stamps/:id', (req, res, next) => {
     const id = parseInt(req.params.id) // id references id in parameter
     const stampIndex = db.stamps.findIndex(s => s.id == id) || {}
     let deletedStamp = {}
     if (stampIndex > -1) {
-        deletedStamp = db.stamps.splice(stampIndex, 1)[0] 
+        deletedStamp = db.stamps.splice(stampIndex, 1)[0]
     }
-    res.json(deletedStamp)
+    res.status(200).json(deletedStamp)
 });
 
 router.put('/stamps/:id', (req, res, next) => {
     const id = parseInt(req.params.id) // id references id in parameter
     const stamp = req.body
-    const stampIndex = db.stamps.findIndex( s => s.id == id)
-    if(stampIndex > -1) {
+    const stampIndex = db.stamps.findIndex(s => s.id == id)
+    if (stampIndex > -1) {
         db.stamps.splice(stampIndex, 1)
     }
     stamp.id = id
     db.stamps.push(stamp)
-    res.json(stamp)
+    res.status(200).json(stamp)
 
 });
 
+
 router.patch('/stamps/:id', (req, res, next) => {
+    const id = parseInt(req.params.id);
+    const stamp = db.stamps.find(s => s.id === id);
+    if (!stamp) {
+        const error = new Error('Stamp not found');
+        error.status = 404;
+        return next(error);
+    }
+    stamp.name = req.body.name || stamp.name;
+    stamp.value = req.body.value || stamp.value;
+    stamp.color = req.body.color || stamp.color;
+    res.status(200).json(stamp);
+});
 
 
+router.post('/stamps', (req, res, next) => {
+    const { name, value, color } = req.body;
+    const newStamp = { id: db.stamps.length + 1, name, value, color };
+    db.stamps.push(newStamp);
+    res.status(201).json(newStamp);
 });
 
 router.get('/stamps', (req, res, next) => {
-    res.json(db.stamps);
+    res.status(200).json(db.stamps);
 
 });
 
