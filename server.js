8f6b21479fc444e7036bf970b561b1bf3f4f3a62
20024416 <n.dinsbach@gmail.com>
2023-03-23 11:40:15 +0100

    installed and imported bodyParser imported routes

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const stampRoutes = require('./routes/stamps')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use('/api/v1', stampRoutes)
+
+
+app.listen(3001);
