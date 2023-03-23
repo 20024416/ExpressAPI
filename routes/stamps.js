e273d4dfdf27df407e75872bbc96dea4d35502f2
20024416 <n.dinsbach@gmail.com>
2023-03-23 12:44:52 +0100

    moved route folder to root, implemented get stamp by id

--- routes/stamps.js
+++ routes/stamps.js
@@ -4,8 +4,9 @@ const db = require('./../db.json')
 
 // RestFul API
 router.get('/stamps/:id', (req, res, next) => {
-    const id = parseInt(req.params.id) // id references id in parameter path
-    
+    const id = parseInt(req.params.id) // id references id in parameter
+    const stamp = db.stamps.find( s => s.id == id) || {}
+    res.json(stamp)
     
     res.status(responsCode).json(stamp)
 });
