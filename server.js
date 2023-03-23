779b77f05423837cc13289b49440d7f5e6f413b8
20024416 <n.dinsbach@gmail.com>
2023-03-23 12:23:42 +0100

    added images and public folder, imported 'path' to create a static folder path

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const path = require('path')
 const stampRoutes = require('./routes/stamps')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use('/', stampRoutes)
 
