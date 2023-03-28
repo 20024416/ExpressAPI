877f1cdd48ab8f6cc7f94edceb4944d282b2462d
20024416 <n.dinsbach@gmail.com>
2023-03-28 15:29:29 +0200

    Installed Cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require ('cors');
 const path = require('path')
 const stampRoutes = require('./routes/stamps')
 const app = express()
@@ -9,15 +9,20 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json()) // parse JSON
 app.use(express.static(path.join(__dirname, "public")))  // create static public folder
+app.use((req, res, next) => {
+res.set('Access-Control-Allow-Origin', '*');
+next();
+
+})
 
 // step through the routes in routes folder
 app.use(stampRoutes)
 
 // catch all | send 404
-// app.use((res, req, next) => {
-//    res.status(404);
+app.use((req, res) => {
+   res.status(404).send(`<h1>Not found <h1>`)
 
-// })
+})
 
 
 app.listen(3001);
